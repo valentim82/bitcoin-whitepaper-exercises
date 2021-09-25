8072fc5f43b4d112cb02ce54f4f9674e359a7b61
valentim82 <ubuntu@ip-172-31-20-251.us-east-2.compute.internal>
2021-09-25 13:32:55 +0000

    hashing exercices - week 1 - Consensys Bootcamp

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -28,16 +28,71 @@ Blockchain.blocks.push({
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+ for (let line of poem) {
+	 createBlock(line)
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+}
+// Create a funcion called createBlock
+
+function createBlock(_data){
+	let block = {
+		index: Blockchain.blocks.length,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length -1].hash,
+		data: _data,
+		timestamp: Date.now()
+	}
+	block.hash = blockHash(block)
+	Blockchain.blocks.push(block)
+	console.log(block)
+	return block
+}
+
+
+function verifyBlock(bl){
+
+	if (bl.data == null) return false; 
+	if (bl.index === 0) {
+		if (bl.hash != "000000") return false;			
+	}
+	else{
+		if (!bl.prevHash) return false; 
+		if (!(
+				typeof bl.index === "number" &&
+				Number.isInteger(bl.index) &&
+				bl.index > 0
+		))
+		{
+			return false;		
+		}
+		if (bl.hash !== blockHash(bl)) return false;
+		if (Array.isArray(bl.data)) return false;
+
+	}
+	return true;
+
+}
+
+function verifyChain(_chain){
+// how walk throught the chain
+	var prevHash; // how I got the prevHash value???
+	for (let bl of _chain.blocks) {
+		if (prevHash && bl.prevHash !==prevHash) return false;
+		if(!verifyBlock(bl)) return false
+	}
+	return true
+
+	
+}
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
 
 // **********************************
 
 function blockHash(bl) {
+	//let block = JSON.stringify(bl);
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		// (index, prevHash, data, and timestampp)
+		`${bl.index};${bl.prevHash};${bl.data};${bl.timestamp};`
 	).digest("hex");
 }
